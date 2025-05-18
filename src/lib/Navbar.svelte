29fbc8e384ba7134a8f21d88cee6212f634e7ccf
mvdwaal <mayravdwaal@gmail.com>
2025-05-18 10:47:12 +0200

    Navbar buttons

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,34 +1,73 @@
 <script>
-  import logoBig from '../assets/logo_big.svg?url';
+  import logoBig from "../assets/logo_big.svg?url";
+
+  function scrollToSection(sectionId) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  }
 </script>
 
 <nav class="navbar">
-  <div class="logo-container">
+  <div class="logo">
     <img src={logoBig} alt="Mayra van der Waal Logo" class="logo" />
+  </div>
+  <div class="buttons">
+    <button class="navigation" on:click={() => scrollToSection("about")}
+      >Over mij</button
+    >
+    <button class="navigation" on:click={() => scrollToSection("portfolio")}
+      >Portfolio</button
+    >
+    <button class="navigation" on:click={() => scrollToSection("contact")}
+      >Contact</button
+    >
   </div>
 </nav>
 
 <style>
   .navbar {
-    position: fixed;
     top: 0;
     left: 0;
     width: 100%;
     height: 80px;
-    background-color: #bcbfa0;  /* Soft mint green color */
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    background-color: #bcbfa0; /* Soft mint green color */
     z-index: 100;
-  }
-
-  .logo-container {
     display: flex;
     align-items: center;
+    position: fixed;
+    justify-content: flex-end;
   }
 
   .logo {
     height: 60px;
     width: auto;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
   }
-</style> 
+
+  .buttons {
+    display: flex;
+    gap: 10px;
+    padding-right: 16px;
+  }
+
+  .navigation {
+    width: 110px;
+    height: 40px;
+    font-size: 15px;
+    border: 1px solid black;
+    color: black;
+    background-color: transparent;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: transform 0.2s;
+    white-space: nowrap;
+  }
+
+  .navigation:hover {
+    transform: translateY(-2px);
+  }
+</style>
